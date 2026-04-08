c372edcf1bf295c787e7b05efdc6fe38367b804a
dileepk706 <dlpkmr706@gmail.com>
2026-04-08 17:33:54 +0530

    feat(onboarding): add checkGymUrl to facade interface

--- src/module/onboarding/domain/onboarding.fcade.interface.ts
+++ src/module/onboarding/domain/onboarding.fcade.interface.ts
@@ -2,6 +2,7 @@ import { CreateGymDto } from '@/module/gym/application/dtos/create-gym.dtos.js';
 
 interface IOnboardingFcade {
   createWorkspaceAndOnboardOwner(data: CreateGymDto & { user_id: string }): Promise<any>;
+  checkGymUrl(url: string): Promise<boolean>;
 }
 
 export default IOnboardingFcade;
