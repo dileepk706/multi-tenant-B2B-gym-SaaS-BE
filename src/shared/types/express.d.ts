03ed2899ca16e1f9301ed59a06f10edf0c673039
dileepk706 <dlpkmr706@gmail.com>
2026-04-12 03:27:04 +0530

    refactor(auth): update authentication middlewares and type definitions
    Added gymMiddleware and updated types for DecodedToken to use optional properties to provide better flexibility during token decoding.

--- src/shared/types/express.d.ts
+++ src/shared/types/express.d.ts
@@ -1,11 +1,11 @@
 export type DecodedToken = {
-  user_id: string | null;
-  staff_id: string | null;
-  member_id: string | null;
-  email: string;
-  gym_id: string | null;
-  tenant_id: string | null;
-  role: 'admin' | 'staff' | 'member';
+  user_id?: string | null;
+  staff_id?: string | null;
+  member_id?: string | null;
+  email?: string | null;
+  gym_id?: string | null;
+  tenant_id?: string | null;
+  role?: 'admin' | 'staff' | 'member' | null;
 };
 
 declare global {
